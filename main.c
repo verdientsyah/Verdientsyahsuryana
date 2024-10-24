6aa7e42a14599564d71212c82e236213429d1b78
verdientsyah <verdientsyahsuryana67@gmail.com>
2024-10-24 21:09:12 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -9,12 +9,12 @@ int main() {
 
     // Cek apakah n lebih besar dari 50
     if (n > 50) {
-        n = n - 25;  // Jika ya, kurangi 25 dari n
+        n = n + 10;  // Jika ya, tambahkan 10 ke n
     } else {
-        n = n + 10;  // Jika tidak, tambahkan 10 ke n
+        n = n - 25;  // Jika tidak, kurangi 25 dari n
     }
 
-    // Tampilkan nilai n setelah modifikasi
+    // Tampilkan nilai n yang telah dimodifikasi
     printf("Nilai n setelah modifikasi: %d\n", n);
 
     return 0;  // Mengakhiri program
